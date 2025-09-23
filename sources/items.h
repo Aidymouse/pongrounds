fe2dade8779389472fbcb70d20d588081ede12eb
Aidan <aidymouse4891@gmail.com>
2025-09-23 16:37:29 +1200

    Add item data

---
+++ sources/items.h
@@ -0,0 +1,16 @@
+#ifndef _ITEMSH_
+#define _ITEMSH_
+
+#include "structs.h"
+
+const char *item_labels[16][32] = {
+	"Expired Panadol",
+	"Cherry Blossom Cloak",
+};
+
+const char *item_descriptions[16][128] = {
+	"Prevents you from losing, once", // Expired Panadol
+	"Lets you dash", // Cherry Blossom Cloak
+};
+
+#endif
