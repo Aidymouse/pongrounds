93c0657d095b16c1e99086a95f04691ca2a00f63
Aidan <aidymouse4891@gmail.com>
2025-09-27 17:02:58 +1200

    refactor controls to own header

---
+++ sources/controls.h
@@ -0,0 +1,10 @@
+// TODO: these should be user defined somewhere
+#define P1_LEFT_KEY 65 // A Key
+#define P1_RIGHT_KEY 68 // D Key
+#define P1_DASH_KEY KEY_SPACE
+#define P1_ACTIVATE_ITEM KEY_LEFT_SHIFT
+
+#define P2_LEFT_KEY 263 // Left Arrow
+#define P2_RIGHT_KEY 262 // Right Arrow
+#define P2_DASH_KEY KEY_O
+#define P2_ACTIVATE_ITEM KEY_P
