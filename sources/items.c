0a35bb562e6a8a7fb0705756614d0d0db3d05be2
Aidan <aidymouse4891@gmail.com>
2025-10-01 23:29:52 +1300

    move item implementation to C file cos now I need it in more than one place

---
+++ sources/items.c
@@ -0,0 +1,45 @@
+
+#include "items.h"
+
+const char *item_labels[] = {
+	"Expired Panadol",
+	"Cherry Blossom Cloak",
+	"Hyper Gonadism",
+	"Chastity Cage",
+	"Nerd Glasses",
+	"Ceremonial Sword",
+	"Time Wizards Chronometer",
+	"Bachelor of Psychology (Hons)",
+	"Russian Secrets",
+	"Legally Distinct Golden Snotch",
+	"Niekro Trading Card",
+//	"Antique Game Console",
+};
+
+const char *item_descriptions[] = {
+	"Prevents you from losing, once", // Expired Panadol
+	"Lets you dash", // Cherry Blossom Cloak
+	"Makes your paddle wider",
+	"Makes your opponents paddle shorter", // Chastity Cage
+	"", // Nerd Glasses
+	"", // Ceremonial Sword
+	"", // Time Wizards Chronometer
+	"", // Bachelor of psychology (hons)
+	"", // Snotch
+	"Chance to launch a knuckeball on bounce", // Niekro Trading Card
+//	"", // Antique Game Console
+};
+
+const char *item_filenames[] = {
+	"", // Expired Panadol
+	"", // Cherry Blossom Cloak
+	"",
+	"", // Chastity Cage
+	"", // Nerd Glasses
+	"", // Ceremonial Sword
+	"time.png", // Time Wizards Chronometer
+	"", // Bachelor of psychology (hons)
+	"", // Snotch
+	"", // Niekro Trading Card
+	"", // Antique Game Console
+};
--- sources/items.h
+++ sources/items.h
@@ -2,34 +2,10 @@
 #define _ITEMSH_
 
 #include "structs.h"
+#include "defines.h"
 
-const char *item_labels[] = {
-	"Expired Panadol",
-	"Cherry Blossom Cloak",
-	"Hyper Gonadism",
-	"Chastity Cage",
-	"Nerd Glasses",
-	"Ceremonial Sword",
-	"Time Wizards Chronometer",
-	"Bachelor of Psychology (Hons)",
-	"Russian Secrets",
-	"Legally Distinct Golden Snotch",
-	"Niekro Trading Card",
-	"Antique Game Console",
-};
-
-const char *item_descriptions[] = {
-	"Prevents you from losing, once", // Expired Panadol
-	"Lets you dash", // Cherry Blossom Cloak
-	"Makes your paddle wider",
-	"Makes your opponents paddle shorter", // Chastity Cage
-	"", // Nerd Glasses
-	"", // Ceremonial Sword
-	"", // Time Wizards Chronometer
-	"", // Bachelor of psychology (hons)
-	"", // Snotch
-	"Chance to launch a knuckeball on bounce", // Niekro Trading Card
-	"", // Snotch
-};
+extern const char *item_labels[];
+extern const char *item_descriptions[];
+extern const char *item_filenames[];
 
 #endif
