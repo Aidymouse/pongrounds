59db0eaf313a39866c0ef96328ccdf2e26f8b92c
Aidan <aidymouse4891@gmail.com>
2025-10-18 17:05:31 +1300

    fix get_angle_distance fn

--- sources/helper.c
+++ sources/helper.c
@@ -19,18 +19,28 @@ void DrawPolygon(const Vector2 *points, int num_points, float thickness, Color c
 	}
 }
 
-/** Gets closest distance in degrees between two angles, assuming negative is rotating anti-clockwise and positive rotating clockwise */
+/** Gets closest distance angle1 needs to change (in degrees) to be pointing in the same direction as angle 2, assuming negative is rotating anti-clockwise and positive rotating clockwise.
+ * NOTE: capped so it won't rotate more than once i.e. shortest rotation to match direction, not literal angle
+ * */
 float get_angle_distance(float angle1, float angle2) {
 
-
-	// Normalize all angles so they're from 0 to 360
+	// Make all angles so they're from 0 to 360 (360 = 0)
+	// 0 = pointing right
 	angle1 = fmod(angle1, 360);
 	angle2 = fmod(angle2, 360);
 	if (angle1 < 0) { angle1 = 360 + angle1; }
 	if (angle2 < 0) { angle2 = 360 + angle2; }
 
+	//printf("Angle1: %.2f, Angle2: %.2f\n", angle1, angle2);
+
 	float dist1 = angle2 - angle1;
-	float dist2 = (angle2+360) - angle1;
+	float dist2 = -(360 - dist1);
+	if (dist1 < 0) {
+		dist2 = 360 + dist1;
+	}
+
+	//printf("Dist1: %.2f, Dist2: %.2f\n", dist1, dist2);
+
 	if (fabsf(dist1) < fabsf(dist2)) { return dist1; } else { return dist2; }
 }
 
@@ -41,4 +51,16 @@ bool is_heading_towards(Vector2 obj_pos, Vector2 obj_dir, Vector2 target_pos) {
 	float ang_target = Vec2GetAngle(to_target);
 	float ang_dir = Vec2GetAngle(obj_dir);
 	return ( fabsf(get_angle_distance(ang_target, ang_dir)) < 90);
+}
+
+int signi(int i) {
+	if (i<0) { return -1; }
+	if (i == 0) { return 0; }
+	return 1;
+}
+
+int singf(float f) {
+	if (f<0) { return -1; }
+	if (f == 0) { return 0; }
+	return 1;
 }
